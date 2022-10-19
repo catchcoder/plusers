d8ed2e95588867cbeef8020742ebf592d2bc1fd8
Chris Hawkins <chris.hawkins@bristol.ac.uk>
2022-10-19 11:05:43 +0100

    Added Commnand Line arguments feature

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -24,18 +24,20 @@ namespace plusers
                     // iterate over members
                     foreach (var p in group.GetMembers())
                     {
-                        Console.WriteLine("{0}: {1}", p.StructuralObjectClass, p.DisplayName);
+                        Console.WriteLine("{0}: {1} - {2}", p.StructuralObjectClass, p.Name, p.DisplayName);
 
                         // do whatever you need to do to those members
                         var theUser = p as UserPrincipal;
 
                         if (theUser != null)
-                            if (theUser.IsAccountLockedOut())
-                            {
-                                //Console.WriteLine ("disabled");
-                            }
-                        //Console.WriteLine("enabled");
+                        {
+                           // Console.WriteLine(theUser.Name);
+                        }
+
+
+
                     }
+            
             }
             catch (NullReferenceException err1)
             {
@@ -46,7 +48,41 @@ namespace plusers
 
         private static void Main(string[] args)
         {
-            GetListOfAdUsersByGroup();
+            if (args.Length == 0)
+            {
+                GetListOfAdUsersByGroup();
+                return ;
+            }
+
+            switch (args[0].ToLower())
+            {
+                case "--full":
+                    //do something
+                    break;
+                case "-f":
+                    goto case "--full";
+                case "--add-user":
+                    //do something
+                    break;
+                case "-a":
+                    goto case "--add-user";
+                case "--remove-user":
+                    //do something
+                    break;
+                case "-r":
+                    goto case "--remove-user";
+                case "--help":
+                    //do something
+                    break;
+                case "-h":
+                    goto case "--help";
+                default:
+                    Console.WriteLine("Not a valid switch");
+                    break;
+            }
+            Console.WriteLine("param {0}", args[0]);
+            Console.ReadKey();
+            return ;
         }
     }
 }
