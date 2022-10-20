4f9fb30c22205140add543ac8a95e44b51269204
Chris Hawkins <chris.hawkins@bristol.ac.uk>
2022-10-20 16:05:46 +0100

    Help Message and Switch chnages

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
 using System.DirectoryServices.AccountManagement;
 
 namespace plusers
@@ -27,16 +26,16 @@ namespace plusers
         {
             try
             {
-                var ctx = new PrincipalContext(ContextType.Domain, Globals.AdDomain);
+                PrincipalContext ctx = new PrincipalContext(ContextType.Domain, Globals.AdDomain);
 
                 // define a "query-by-example" principal - here, we search for a GroupPrincipal
-                var group = GroupPrincipal.FindByIdentity(ctx, Globals.AdSecurityGroup);
+                GroupPrincipal group = GroupPrincipal.FindByIdentity(ctx, Globals.AdSecurityGroup);
 
 
                 // if found....
                 if (group != null)
                     // iterate over members
-                    foreach (var p in group.GetMembers())
+                    foreach (Principal p in group.GetMembers())
                         if (stype == "email")
                             Console.WriteLine("{0}: Login: {1} - Name: {2} Email:{3} ", p.StructuralObjectClass, p.Name,
                                 p.DisplayName, p.UserPrincipalName);
@@ -58,68 +57,79 @@ namespace plusers
 
         private static void Main(string[] args)
         {
-            
-            if (args.Length == 0)
+            try
             {
-                GetListOfAdUsersByGroup("basic");
-                return;
-            }
 
-            //capture Command Arguments 
-            Globals.CommandSwitches[0] = args[0].ToLower().Trim();
-            if (args.Length == 2)
+                Console.WriteLine("PL User v0.1 - Tool to manage users in a Security Group\n");
+
+                if (args.Length == 0)
+                {
+                    GetListOfAdUsersByGroup("basic");
+                    return;
+                }
+
+                //capture Command Arguments 
+                Globals.CommandSwitches[0] = args[0].ToLower().Trim();
+                if (args.Length == 2)
+                {
+                    Globals.CommandSwitches[1] = args[1].ToLower().Trim();
+
+                }
+
+                switch (args[0].ToLower())
+                {
+                    case "--email-addresses":
+                        //Display Users and email addresses
+                        GetListOfAdUsersByGroup("full");
+                        break;
+                    case "--add-user":
+                        //Check username provided
+                        if (CheckArgumentUsername() == false)
+                            //Add user to the security group
+                            PlusersAddUser();
+                        break;
+                    case "-a":
+                        goto case "--add-user";
+                    case "--remove-user":
+                        //Remove user from Security group
+                        PlusersRemoveUser();
+                        break;
+                    case "-r":
+                        goto case "--remove-user";
+                    case "--help":
+                        //Displays help
+                        PlusersHelp();
+                        break;
+                    case "-h":
+                        goto case "--help";
+                    default:
+                        Console.WriteLine("Not a valid switch");
+                        break;
+                }
+
+                Console.ReadKey();
+            }
+            catch (Exception e)
             {
-                Globals.CommandSwitches[1] = args[1].ToLower().Trim();
-
+                Console.WriteLine("Error: {0}", e);
+                throw;
             }
-
-            switch (args[0].ToLower())
-            {
-                case "--email-address":
-                    //Display Users and email addresses
-                    GetListOfAdUsersByGroup("full");
-                    break;
-                case "-e":
-                    goto case "--email-address";
-                case "--add-user":
-                    //Check username provided
-                    if (CheckArgumentUsername() ==false)
-                    //Add user to the security group
-                    PlusersAddUser();
-                    break;
-                case "-a":
-                    goto case "--add-user";
-                case "--remove-user":
-                    //Remove user from Security group
-                    PlusersRemoveUser();
-                    break;
-                case "-r":
-                    goto case "--remove-user";
-                case "--help":
-                    //Displays help
-                    PlusersHelp();
-                    break;
-                case "-h":
-                    goto case "--help";
-                default:
-                    Console.WriteLine("Not a valid switch");
-                    break;
-            }
-
-           // Console.ReadKey();
         }
 
         private static void PlusersHelp()
         {
-            var sb = new System.Text.StringBuilder("This is used to add users to a security group \"" + Globals.AdSecurityGroup + "\" for SSH Access\r\n");
+            System.Text.StringBuilder sb = new System.Text.StringBuilder("This is used to add users to a Security Group \"{0}\" for SSH Access\r\n\r\n");
+            //sb.Append("Copyright (c) 2022 Chris Hawkins\r\n");
+            sb.Append("Usage:\r\n");
+            sb.Append("plusers.exe [option] <user name>\r\n\r\n");
             sb.Append("Command line options:\r\n");
             sb.Append("\t-a , --add-user \tAdd user to the security\r\n");
-            sb.Append("\t-e , --email-address \tDisplay emails addresses of the users within the security\r\n");
-            sb.Append("\t-r , --remove-user \tRemove user from security group\r\n");
-            sb.Append("\t-h , --help \t\tDisplays help, this.\r\n");
+            sb.Append("\t-r , --remove-user \tRemove user from security group\r\n\r\n");
+            sb.Append("\t--email-address \tDisplay emails addresses of the users within the security\r\n");
 
 
-            Console.WriteLine (sb.ToString());
+
+            Console.WriteLine(sb.ToString(), Globals.AdSecurityGroup);
 
         }
         private static void PlusersRemoveUser()
